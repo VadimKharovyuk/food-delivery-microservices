1a16859cf515b19827fb5790a7d7c2cd5880081a
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-06-28 11:36:32 +0300

    DoneV1

--- src/main/java/com/example/deliveryproductservice/DeliveryProductServiceApplication.java
+++ src/main/java/com/example/deliveryproductservice/DeliveryProductServiceApplication.java
@@ -8,7 +8,7 @@ public class DeliveryProductServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DeliveryProductServiceApplication.class, args);
-        System.out.println("hello world");
+
     }
 
 }
